336bc34fd27bd200a5f539457a33d80c3eb0e39e
sanjay <vsanjay8722@gmail.com>
2023-01-24 19:17:41 +0530

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const cors=require("cors");
-const {connection}=require("./configs/db");
+// const {connection}=require("./configs/db");
 const {userRouter}=require("./routes/user.route");
 const {adminRouter}=require("./routes/admin.route");
 const {productRouter}=require("./routes/products.route");
@@ -8,10 +9,21 @@ const {authenticate}=require("./middlewares/authenticate.middleware");
 const {cartRouter}=require("./routes/cart.route")
 require("dotenv").config();
 const app=express();
+
 app.use(cors({
     origin:"*"
 }))
 app.use(express.json());
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.mongourl);
+      console.log("MongoDB Connected");
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
 
 app.get("/",(req,res)=>{
     res.send("WELCOME")
@@ -25,13 +37,18 @@ app.use("/cart",cartRouter)
 
 
 const port=process.env.port;
-app.listen(port,async(req,res)=>{
-    try {
-        await connection;
-        console.log("Connected to DB");
-    } catch (error) {
-        console.log(error);
-        res.send({"err":"Something went wrong while connecting to db"});
-    }
-    console.log(`The server is running at ${port}`)
+// app.listen(port,async(req,res)=>{
+//     try {
+//         await connection;
+//         console.log("Connected to DB");
+//     } catch (error) {
+//         console.log(error);
+//         res.send({"err":"Something went wrong while connecting to db"});
+//     }
+//     console.log(`The server is running at ${port}`)
+// })
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`The server is running at ${port}`);
+    })
 })
